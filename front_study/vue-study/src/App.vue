6169031b935af061de30ce75fd105930513aa706
saturn_wh <11731797+saturn_wh@user.noreply.gitee.com>
2025-06-28 22:19:21 +0800

    创建一个学习项目，实验代码用

---
+++ front_study/vue-study/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+    import Person from './components/Person.vue';
+
+</script>
+
+<template>
+  <Person/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ front_study/vue-study/src/components/Person.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="person">
+        <h1>姓名：{{name}}</h1>
+        <h1>年龄：{{age}}</h1>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person123">
+    let name = '张三'
+
+    let age = 18
+</script>
+
+<style scoped>
+    .person{
+        background-color: aqua;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 10px;
+    }
+</style>
